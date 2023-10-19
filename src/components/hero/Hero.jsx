98a159f3b86102a1f5315abbc4a0e461fd73d047
Hernán Octavio Rojas <104470156+Spuknan@users.noreply.github.com>
2023-10-19 08:27:05 -0300

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
       <div className="text-white text-center md:text-start z-10 container px-6 mx-auto">
         <div className="max-w-xl">
           <h1 className="text-2xl md:text-4xl font-bold mb-4 glow">HALLOWEEN EN LA COMUNIDAD</h1>
-          <p className="text-md md:text-lg text-zinc-300">Todas las personas que se suscriban en twitch durante el mes de Octubre van a poder elegir en qué parte del fantasmita va a ir su nombre, para quedar en la repisa de los fantasmitas para siempre. No te olvides de pasar por el canal para enterarte de más!</p>
+          <p className="text-md md:text-lg text-zinc-300">Todas las personas que se suscriban en mi canal de twitch durante el mes de Octubre van a estar en el fantasmita! Quedando en la repisa de los fantasmitas para siempre, inmortalizado como un no pleb. No te olvides de pasar por el canal para enterarte de más!</p>
         </div>
       </div>
     </div>
