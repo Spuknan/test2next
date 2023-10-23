405f9b6bf06cfe20bf4d55112e6994e08b8825c8
Hernán Rojas <horojas96@gmail.com>
2023-10-23 16:49:58 -0300

    npm i

--- src/app/marcas/page.jsx
+++
@@ -1,7 +0,0 @@
-const Marcas = () => {
-  return (
-    <div className="bg-zinc-800 h-screen">Marcas</div>
-  )
-}
-
-export default Marcas
--- src/components/stats/TwitchStats.jsx
+++ src/components/stats/TwitchStats.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 const TwitchStats = () => {
   return (
     <div>TwitchStats</div>
