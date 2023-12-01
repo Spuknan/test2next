528d8fea5ac6f949a913ec57b095875f86d3cbfb
Hernán Rojas <horojas96@gmail.com>
2023-12-01 10:31:00 -0300

    christmas

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       {/* Fondo de la imagen */}
       <div
         className="absolute inset-0 w-full h-full bg-cover bg-center"
-        style={{ backgroundImage: 'url("/images/halloween.jpg")' }}
+        style={{ backgroundImage: 'url("/images/navidad.png")' }}
       >
         {/* Overlay oscuro */}
         <div className="absolute inset-0 bg-black opacity-70"></div>
@@ -17,8 +17,9 @@ const Hero = () => {
       {/* Contenido centrado */}
       <div className="text-white text-center md:text-start z-10 container px-6 mx-auto">
         <div className="max-w-xl">
-            <h1 className="text-2xl md:text-4xl font-bold mb-4 glow">HALLOWEEN EN LA COMUNIDAD</h1>
-            <p className="text-md md:text-lg text-zinc-300">Todas las personas que se suscriban en mi canal de twitch durante el mes de Octubre van a estar en el fantasmita! Quedando en la repisa de los fantasmitas para siempre, inmortalizado como un no pleb. No te olvides de pasar por el canal para enterarte de más!</p>
+            <h1 className="text-2xl md:text-4xl font-bold mb-4 glow">LLEGÓ LA NAVIDAD AL SÓTANO</h1>
+            <p className="text-md md:text-lg text-zinc-300">Otro año más pasando juntxs la navidad! Todas las personitas que se subeen durante Diciembre van a ir anotadas en unos hermosos unicornios que van en el arbol!</p>
+            <p className="text-md md:text-lg text-zinc-300 mt-2">Querés ir en la estrella? Entrá al stream y enterate de cómo hacer!</p>
         </div>
       </div>
     </div>
